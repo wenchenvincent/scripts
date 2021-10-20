af7b287f102cee33ca813c839565211d24f1ebbe
Wen Chen <Wen.Chen@amd.com>
2021-10-20 21:54:47 +0000

    Added grid to the plot for MLPerf Resnet50

--- mlperf/plot_trends_resnet50.py
+++ mlperf/plot_trends_resnet50.py
@@ -18,35 +18,39 @@ def get_loss_and_accuracy(lines, train=True):
             array.append([float(match.group(1)), float(match.group(2)), float(match.group(3))])
     return np.array(array)
 
-def get_batch_from_step(step, num_of_gpus=4, batch_size_per_gpu=256, num_of_training_samples=1281167):
+def get_batch_from_step(step, num_of_gpus=8, batch_size_per_gpu=256, num_of_training_samples=1281167):
     global_batch_size = num_of_gpus * batch_size_per_gpu
     steps_per_epoch = num_of_training_samples / global_batch_size + 1
     epoch = step / steps_per_epoch
     return epoch
 
-def plot_trends(train_metrics, test_metrics,fmt='.-b'):
+def plot_trends(train_metrics, test_metrics, legend, fmt='.-b'):
     fig, ax = plt.subplots(2,2,figsize=(10,7))
     fig.suptitle("MLPerf Resnet50")
     ax[0][0].plot(train_metrics[:,0], train_metrics[:,2], fmt)
     ax[0][0].set_xlabel('Epoch')
     ax[0][0].set_ylabel('Training Accuracy')
     ax[0][0].grid()
+    ax[0][0].legend([legend])
 
 
     ax[0][1].plot(train_metrics[:,0], train_metrics[:,1], fmt)
     ax[0][1].set_xlabel('Epoch')
     ax[0][1].set_ylabel('Training Loss')
     ax[0][1].grid()
+    ax[0][1].legend([legend])
 
     ax[1][0].plot(test_metrics[:,0], test_metrics[:,2], fmt)
     ax[1][0].set_xlabel('Epoch')
     ax[1][0].set_ylabel('Test Accuracy')
     ax[1][0].grid()
+    ax[1][0].legend([legend])
 
     ax[1][1].plot(test_metrics[:,0], test_metrics[:,1], fmt)
     ax[1][1].set_xlabel('Epoch')
     ax[1][1].set_ylabel('Test Loss')
     ax[1][1].grid()
+    ax[1][1].legend([legend])
 
 
     fig.savefig('trends.png')
@@ -57,8 +61,7 @@ if __name__ == '__main__':
         lines = fi.readlines()
         train_metrics = get_loss_and_accuracy(lines)
         test_metrics = get_loss_and_accuracy(lines,train=False)
-        print(test_metrics)
         train_metrics[:,0] = np.vectorize(get_batch_from_step)(train_metrics[:,0])
         test_metrics[:,0] = np.vectorize(get_batch_from_step)(test_metrics[:,0])
-        plot_trends(train_metrics,test_metrics)
+        plot_trends(train_metrics,test_metrics, sys.argv[2])
 
