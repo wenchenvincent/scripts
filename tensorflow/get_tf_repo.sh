f9f98f57cebe8cf263c9f095dad50632a9b7e6dc
wenchenvincent <32376000+wenchenvincent@users.noreply.github.com>
2021-10-20 16:59:53 -0500

    Create get_tf_repo.sh

---
+++ tensorflow/get_tf_repo.sh
@@ -0,0 +1,3 @@
+cd /root && \git clone https://github.com/ROCmSoftwarePlatform/tensorflow-upstream tensorflow
+
+cd /root/tensorflow && git remote add google_upstream https://github.com/tensorflow/tensorflow.git && git fetch google_upstream
