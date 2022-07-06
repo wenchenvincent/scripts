3abf04f24b3f881ebee6bdee17ad9436a8b4a53b
Wen Chen <Wen.Chen@amd.com>
2022-07-06 19:37:35 +0000

    Correct the formular for calculating norm error and refactor code a bit

--- misc/calculate_norm.py
+++ misc/calculate_norm.py
@@ -9,6 +9,19 @@ def load_matrix_from_file(file_name, m, n, data_type, col_major=True):
         matrix = np.reshape(data, (m, n))
     return matrix
 
+def calculate_norm_error(C, ref_C, norm_type):
+    return np.linalg.norm(C.astype(np.float64)-ref_C.astype(np.float64), ord=norm_type) / np.linalg.norm(ref_C.astype(np.float64), ord=norm_type)
+
+
+def calculate_relative_error(C, ref_C):
+    relative_error = np.abs(C-ref_C) / np.abs(ref_C)
+    idx = np.argmax( relative_error)
+    relative_error = np.amax(relative_error)
+
+    return relative_error, idx
+    
+
+
 # The memory layout of the data is column major
 A = load_matrix_from_file('A.dat', 512, 512, np.float16) 
 B = load_matrix_from_file('B.dat', 512, 2700, np.float16)
@@ -18,23 +31,24 @@ ref_C = np.dot(A,B)
 print('ref_C=', ref_C)
 print('C=', C)
 
-norm1 = np.linalg.norm(C-ref_C, ord=1)
+norm1 = calculate_norm_error(C, ref_C, 1)
 print('norm 1=', norm1)
-norm_f = np.linalg.norm(C-ref_C)
+norm_f = calculate_norm_error(C, ref_C, 'fro')
 print('Frobenius norm=', norm_f)
 
-relative_error = np.abs(C-ref_C)/np.abs(ref_C) 
-idx = np.argmax( np.abs(C-ref_C)/np.abs(ref_C) )
+relative_error, idx = calculate_relative_error(C, ref_C)
 y = idx // 2700
 x = idx % 2700
-print('relative error is', relative_error[y][x])
+print('relative error is', relative_error)
 print('C[%d][%d]=' % (y, x), C[y][x])
 print('ref_C[%d][%d]=' % (y, x), ref_C[y][x])
 
-relative_error = np.amax(relative_error)
-print('relative error=', relative_error)
 
 eps = 1.0/1024
 threshold = 50
 error_bound = max(512, 2700) * eps * threshold
 print('error_bound=', error_bound)
+
+#relative_error_bound = np.sqrt(2700) * eps
+relative_error_bound = 2 * 2700 * eps
+print('relative_error_bound=', relative_error_bound)
