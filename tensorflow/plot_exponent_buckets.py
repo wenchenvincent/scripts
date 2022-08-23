a425c39baaea65c02abf9d865b4eb7682d0f3376
Wen Chen <Wen.Chen@amd.com>
2022-08-23 21:42:04 +0000

    Add percentile calculation in the histogram

--- tensorflow/plot_exponent_buckets.py
+++ tensorflow/plot_exponent_buckets.py
@@ -1,14 +1,13 @@
 import sys
 import re
+import numpy as np
 import matplotlib.pyplot as plt
 
 def extract_buckets(lines):
     bucket_dict = {}
     for line in lines:
-        #match = re.search(r'(\w+)\s+\=\s+\[(\w+)\]', line)
         match = re.search(r'(\w+)\s+\=\s+\[([\w,]+)\]', line)
         if match:
-            print(line)
             title = match.group(1)
             buckets = match.group(2)
             buckets = buckets.split(',')
@@ -18,13 +17,25 @@ def extract_buckets(lines):
                 bucket_dict[title] = buckets
     return bucket_dict
 
+def compute_percentile_range(buckets, percentile):
+    '''
+    Calculate the index of the bucket corresponding to the percentile
+    '''
+    histogram = np.array(buckets)
+    total =  histogram.sum()
+    cdf = np.cumsum(histogram / total)
+    idx = np.searchsorted(cdf, percentile / 100)
+    return idx
 
 def plot_histogram(title, buckets):
     plt.figure(figsize=(20,12))
     plt.title(title)
     x = list(range(-24, 16))
     x = ['zero'] + [str(i) for i in x] + ['inf', 'nan']
-    plt.bar(x, buckets)
+    low_idx = compute_percentile_range(buckets, 0.001)
+    high_idx = compute_percentile_range(buckets, 99.999)
+    colors = ['orange'] * (low_idx+1)  + ['blue'] * (high_idx-low_idx-1) + ['orange'] * (len(buckets)-high_idx)
+    plt.bar(x, buckets, color=colors)
     plt.xlabel('power of two')
     plt.ylabel('bucket counts')
     plt.savefig(title+'.png')
