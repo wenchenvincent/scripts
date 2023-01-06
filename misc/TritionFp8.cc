a7577db6779b222ac4610b1c2eb90a520a2eb33d
Wen Chen <Wen.Chen@amd.com>
2023-01-06 23:10:32 +0000

    Fixed typo

--- misc/TritionFp8.cc
+++ misc/TritionFp8.cc
@@ -1,4 +1,5 @@
 
+__device__
 uint32_t cast_fp16X4_to_fp8X4(uint32_t fp16X2_1, uint32_t fp16X2_2) {
   uint32_t a0 = fp16X2_1 << 1;
   uint32_t a1 = fp16X2_2 << 1;
@@ -8,7 +9,7 @@ uint32_t cast_fp16X4_to_fp8X4(uint32_t fp16X2_1, uint32_t fp16X2_2) {
   a1 = a1 + 0x00800080;
   uint32_t b0 = (fp16X2_1 & 0x80008000) | a0;
   uint32_t b1 = (fp16X2_2 & 0x80008000) | a1;
-  unit32_t fp8X4 = (a1 & 0xff000000) |  ( (a1 & 0x0000ff00) << 8 ) | ( (a0 & 0xff000000) >> 16 ) | ( (a0 & 0x0000ff00) >> 8 );
+  uint32_t fp8X4 = (a1 & 0xff000000) |  ( (a1 & 0x0000ff00) << 8 ) | ( (a0 & 0xff000000) >> 16 ) | ( (a0 & 0x0000ff00) >> 8 );
   return fp8X4;
 }
 
@@ -17,6 +18,7 @@ struct fp16X4vec {
    uint32_t fp16X2_1;
 };
 
+__device__
 fp16X4vec cast_fp8X4_to_fp16X4(uint8_t fp8X4) {
   uint32_t a0 = ( (fp8X4 & 0x0000ff00) << 16 ) | ( (fp8X4 & 0x000000ff) << 8 );
   uint32_t a1 = (fp8X4 & 0xff000000) | ( (fp8X4 & 0x00ff0000) >> 8 );
