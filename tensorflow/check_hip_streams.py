0963ce0732c9982c6217b6ca4a24e93fc68461b5
Wen Chen <wenchenvincent@gmail.com>
2024-04-10 06:19:47 +0000

    Check max number of hip streams on one gpu.

--- tensorflow/check_hip_streams.py
+++ tensorflow/check_hip_streams.py
@@ -2,26 +2,32 @@ import sys
 import re
 
 def process_logs(filename):
+    pattern_device = re.compile(r'hipSetDevice \( (\d+) \)')
     pattern_create = re.compile(r'hipStreamCreateWithFlags: Returned hipSuccess : stream:(0x[0-9a-fA-F]+)')
     pattern_destroy = re.compile(r'hipStreamDestroy \( stream:(0x[0-9a-fA-F]+) \)')
 
-    address_set = set()
+    address_set = {}
     max_set_size = 0
 
     with open(filename, 'r') as file:
         for line in file:
+            match_device = pattern_device.search(line)
             match_create = pattern_create.search(line)
             match_destroy = pattern_destroy.search(line)
 
+            if match_device:
+                device = match_device.group(1)
+                if device not in address_set:
+                    address_set[device] = set()
+
             if match_create:
                 address = match_create.group(1)
-                address_set.add(address)
-                max_set_size = max(max_set_size, len(address_set))
-                streams += 1
+                address_set[device].add(address)
+                max_set_size = max(max_set_size, len(address_set[device]))
             elif match_destroy:
                 address = match_destroy.group(1)
-                if address in address_set:
-                    address_set.remove(address)
+                if address in address_set[device]:
+                    address_set[device].remove(address)
 
     return max_set_size
 
